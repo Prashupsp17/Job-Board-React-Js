28bf110f0f2ae4cc22c5ed032c647b5c54add6a6
Prashupsp17 <shindeprashant967@gmail.com>
2024-03-10 21:59:34 +0530

    Job-Board-react-js

---
+++ Job-board-reactjs/src/components/JobPosting.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const JobPosting = ({url,title,by,time}) => {
+    const formattedTime  = new Date(time*1000).toLocaleString();
+  return (
+      <>
+      <div className="post" role="listitem">
+          <h2 className="post__title">
+              <a 
+              className={url ? "": "inactiveLink"}
+              href={url}
+              target="_blank"
+              rel='noopener'
+              >{title}</a>
+          </h2>
+          <span className="post__metadata">
+              By {by} . {formattedTime}
+          </span>
+      </div>
+      </>
+
+  )
+}
+
+export default JobPosting
